4987d6194f494bedcd8c42b0922ff25808920a21
jawahar jovah <jawaharjovah2003@gmail.com>
2025-01-14 21:41:47 +0530

    large commit

--- algo.py
+++ algo.py
@@ -1,12 +1,15 @@
 from surprise.model_selection import cross_validate,train_test_split
 from surprise import Dataset,Reader,accuracy
 from surprise import SVD, BaselineOnly,get_dataset_dir,KNNBaseline
-import re
+import os
+import pandas as pd
+
+df = pd.read_csv(os.getcwd()+"/movie_list.csv")
 
 class Movie_rec():
     # def __init__(self):
     #     pass
-    def Knn():
+    def Knn(n,sw,k=10):
             
         def read_item_name():
             file_name = get_dataset_dir() + "/ml-100k/ml-100k/u.item"
@@ -25,51 +28,59 @@ class Movie_rec():
         sim_options = {"name": "pearson_baseline", "user_based": False}
         algo = KNNBaseline(sim_options=sim_options)
         algo.fit(trainset)
-        df = list([])
-        names = list([])
-        with open(r"C:\Users\JAWAHAR JOVAH/.surprise_data//ml-100k/ml-100k/u.item","r") as f:
-            for i in f.readlines():
-                text = i.split("|",maxsplit=3)
-                for i in text:
-                    df.append(i)
-            for i in range(1,len(df),4):
-                pat1 = re.compile(r"(\w.+)")
-                pat2 = re.compile(r"([a-zA-Z\W\d]+).\(")
-                if pat2.match(df[i]):
-                    name = pat2.findall(df[i])
-                    names.append(name[0])
-                    # print(name[0],"uploaded success..")
-                else:
-                    name = pat1.findall(df[i])
-                    names.append(name[0])
-                    # print(name[0],"uploaded success..")
-
-
+        
         # Read the mappings raw id <-> movie name
         rid_to_name, name_to_rid = read_item_name()
 
-        # Retrieve inner id of the movie Toy Story
-        toy_story_raw_id = name_to_rid["Four Rooms (1995)"]
-        toy_story_inner_id = algo.trainset.to_inner_iid(toy_story_raw_id)
+        matches,matlis = Movie_rec.search(sw)
+        print("THIS IS n VALUE     : ",n)
+        print("THIS IS REQ_VALUE   : ",sw)
+        print("THIS IS matlis      : ",matlis)
+        print("THIS IS matches     : ",matches)
 
-        # Retrieve inner ids of the nearest neighbors of Toy Story.
-        toy_story_neighbors = algo.get_neighbors(toy_story_inner_id, k=10)
+        movie_neighbors_names = []
+        movie_neighbors_rawIds = []
+       
+        if n:
+            # Retrieve inner id of the movie Toy Story
+            movie_raw_id = name_to_rid[n]
+            movie_inner_id = algo.trainset.to_inner_iid(movie_raw_id)
 
-        # Convert inner ids of the neighbors into names
-        toy_story_neighbors_rawIds = []
-        for i in toy_story_neighbors:
-            toy_story_neighbors_rawIds.append(algo.trainset.to_raw_iid(i))
+            # Retrieve inner ids of the nearest neighbors of Toy Story. 
+            movie_neighbors = algo.get_neighbors(movie_inner_id, k=k)
 
-        toy_story_neighbors_names = []
-        for rid in toy_story_neighbors_rawIds:
-            toy_story_neighbors_names.append(rid_to_name[rid])
+            # Convert inner ids of the neighbors into names
+            for i in movie_neighbors:
+                movie_neighbors_rawIds.append(algo.trainset.to_raw_iid(i))
 
+            for rid in movie_neighbors_rawIds:
+                movie_neighbors_names.append(rid_to_name[rid])
 
-        # print("The 10 nearest neighbors of Toy Story are:")
-        # for movie in toy_story_neighbors_names:
-        #     print(movie)
+        return movie_neighbors_names,sorted(matlis),k
+        
+    
+    def search(n):
+        
+        matlis = list()
+        matches = list()
+        # print(df["movie"])
+        if n:
+            print("running search operations......")
+            matches = sorted([i for i in df["movie"] if n==(i[0:len(n)]).lower()])
+            if matches:
+                matlis = map(lambda x:df[df["movie"] == x]["name"].tolist()[0],matches)
+            else:
+                print("MATCH NOT FOUND!")
+                matlis = df["name"]
+        else:
+            print("ENTER SOMETHING!")
+            matlis = df["name"]
 
-        return toy_story_neighbors_names
+        return matches,list(matlis)
+    
+    def all_movies():
+        return sorted(df["name"])        
+
 
 if __name__ == '__main__':
-    Movie_rec
+    Movie_rec.search()
--- main.py
+++ main.py
@@ -6,16 +6,35 @@ MR = Movie_rec
 
 @app.route("/")
 def home():
-    # a = MR.Knn(input("Enter the input:"))
-    return render_template("Home.html")
+    matlis = MR.all_movies()
+    return render_template("Home.html",matlis = matlis)
 
 @app.route("/Home", methods=['POST','GET'])
 def result():
     if request.method == 'POST':
-        result = request.form
-        tsl = MR.Knn()
-        # print(tsl)
-        return render_template("Home.html",result = result,tsl=tsl)
+        req_name = request.form.get('Movie_Name')
+        top_num = request.form.get('Top_num')
+        Lom = request.form.get("Lom")
+        tsl = MR.all_movies()
+        matlis = MR.all_movies()
+        
+        if Lom:
+            tsl,matlis,K = MR.Knn(Lom,req_name,k=int(top_num))
+            # print(" THIS IS tsl :",tsl)
+            # print(" THIS IS matlis :",matlis)
+
+            return render_template("Home.html",
+                                   Lom = Lom,
+                                   tsl=tsl,
+                                   matlis=matlis,
+                                   req_name=req_name,
+                                   top_num = K
+                                   
+                                   )
+        
+        else:
+            return render_template("Home.html",matlis = matlis)
+    return render_template("Home.html",matlis = matlis)
 
 if __name__ == '__main__':
     app.run(debug=True)
